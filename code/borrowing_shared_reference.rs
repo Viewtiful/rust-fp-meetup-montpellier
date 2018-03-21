ef8134d12f3a197caf2b5852b470f7f429c822ba
Baptiste HAUDEGAND <baptiste.haudegand@gmail.com>
2018-03-21 19:20:38 +0100

    borrowing shared reference example

---
+++ code/borrowing_shared_reference.rs
@@ -0,0 +1,13 @@
+fn print_vec(vec: &Vec<i32>) {
+    println!("{:?}", vec);
+}
+
+fn main() {
+    let mut vec: Vec<i32> = Vec::new();
+    vec.push(1);
+    vec.push(2);
+
+    print_vec(&vec);
+
+    println!("{:?}", vec);    
+}
