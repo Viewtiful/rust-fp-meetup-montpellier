2ec4c9292f3cc564a3093329ae321fb764e9fba5
Baptiste HAUDEGAND <baptiste.haudegand@gmail.com>
2018-03-21 19:03:36 +0100

    format file

--- code/ownership_1.rs
+++ code/ownership_1.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let my_vec: Vec<i32> = vec![1, 2, 3];
+    let my_vec: Vec<i32> = vec![1, 2, 3];   // allocated here
 
-    println!("{:?}", my_vec);
+    println!("{:?}", my_vec);               // dropped here
 }
